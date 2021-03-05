f4ef3200970e91c032d53d60d0c868da1936b69c
Kuan Hui, Wu <tf00604520@gmail.com>
2021-03-05 19:32:47 +0800

    unknown update

---
+++ src/jumbotron.js
@@ -0,0 +1,13 @@
+import './jumbotron.css';
+
+
+
+const jumbotron = () => {
+    return(
+        <div className="jumbotron">
+            <h1>Welecom to DevCraft.</h1>
+            <p>Learn more and code more.</p>
+            <button>I'm feeling lucky.</button>
+        </div>
+    )
+}
