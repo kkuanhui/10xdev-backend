74bbfef6cfb0e53a468b60be679d095c4473219e
Kuan Hui, Wu <tf00604520@gmail.com>
2021-03-21 12:06:41 +0800

    some update

---
+++ src/SideBar.js
@@ -0,0 +1,15 @@
+
+const SideBar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Index</li>
+        <li>About Me</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+
+  );
+}
+
+export default SideBar;
